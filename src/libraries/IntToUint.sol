fc34f188201c420da5d8bb3129fd47f64156ed28
Pumipat Watanakulcharus <pooh99191@gmail.com>
2022-07-02 21:27:02 +0700

    0 case

--- src/libraries/IntToUint.sol
+++ src/libraries/IntToUint.sol
@@ -4,7 +4,7 @@ import "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 library IntToUint {
     function toString(int value) internal pure returns (string memory) {
-        if (value > 0) return Strings.toString(uint(value));
+        if (value >= 0) return Strings.toString(uint(value));
         else return string.concat("-", Strings.toString(uint(0-value)));
     }
     function tostringwithout(int value) internal pure returns (string memory) {
